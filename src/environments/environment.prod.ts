810c749d49085e1290b1d985a17ba7ef7d029fa6
riteshthakre1 <riteshthakre976@gmail.com>
2026-05-14 11:24:07 +0530

    environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://deploy.up.railway.app',
-  razorpayKey: 'rzp_live_REPLACE_WITH_LIVE_KEY'
+  apiUrl: 'https://deploy-production-63e1.up.railway.app',
+  razorpayKey: 'rzp_test_Skpqod86o0oEyJ'
 };
